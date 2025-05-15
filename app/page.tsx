7155cd18644ff22bcc3f925a6f8d4c65c0f4f647
Jawad Khane <jawadkhan.cse@gmail.com>
2025-05-15 22:39:30 +0500

    Implement ESlint and Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl text-violet-700 font-black">Welocome to Next fullstack 👋</h1>
+      <h1 className="text-3xl text-violet-700 font-black">
+        Welocome to Next fullstack 👋
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
