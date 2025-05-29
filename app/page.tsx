2fb92fc6b12546b9b270c17819b35c19def4fbf9
Jawad Khane <jawadkhan.cse@gmail.com>
2025-05-29 20:28:58 +0500

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ const Home = () => {
   return (
     <div className="background-light850">
       <h1 className="h1-bold text-dark-500">Welocome to Next fullstack 👋</h1>
+      <h1 className="h1-bold text-dark-500 font-space-grotesk">
+        Welocome to Next fullstack 👋
+      </h1>
     </div>
   );
 };
