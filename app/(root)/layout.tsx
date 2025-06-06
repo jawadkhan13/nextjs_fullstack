93c411f9f6e99fb459dd6c2a173f0adbe138b33b
Jawad Khane <jawadkhan.cse@gmail.com>
2025-06-06 22:37:26 +0500

    relocate navigation folder

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import Navbar from "@/components/ui/navigation/navbar";
+import Navbar from "@/components/navigation/navbar";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,4 @@
 import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 
 const Home = async () => {
@@ -16,9 +15,7 @@ const Home = async () => {
 
           await signOut({ redirectTo: ROUTES.SIGN_IN });
         }}
-      >
-        <Button type="submit">Log out</Button>
-      </form>
+      ></form>
     </>
   );
 };
